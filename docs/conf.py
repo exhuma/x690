4fccc52d29ecb5fa17e36214786f13535a354e3c
Michel Albert <michel@albert.lu>
2020-12-07 10:38:50 +0100

    Fix version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Michel Albert"
 author = "Michel Albert"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
